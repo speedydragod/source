a41a8bd2bee156a045c82a940ae2915c1ca2a5fd
speedydragod <851375627@qq.com>
2017-05-15 15:52:23 +0800

    Create UnSymmetricEncrypt.java

---
+++ com/encrypt/UnSymmetricEncrypt.java
@@ -0,0 +1,20 @@
+package com.encrypt;
+/**
+ * 单向加密
+ * @author kai.long
+ *	MD5 Base64 SHA-1(弃用) SHA-2
+ */
+public class UnSymmetricEncrypt {
+
+	private static final String Base64="Base64";
+	private static final String MD5="MD5";
+	private static final String SHA_1="SHA-1";
+	
+	public static void MD5(){
+		
+		
+	}
+	
+}
+
+
