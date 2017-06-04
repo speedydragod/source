b62705324458f18e72c45f72532f2415a31c19b5
speedydragod <851375627@qq.com>
2017-06-04 23:15:09 +0800

    Add files via upload

---
+++ com/model/FilterPattern/CompositePattern/Employee.java
@@ -0,0 +1,64 @@
+package com.model.CompositePattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+
+	private String name;
+	private String dept;
+	private int salary;
+	
+	private List<Employee> subordinates;
+
+	public Employee(String name, String dept, int salary) {
+		super();
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+		this.subordinates = new ArrayList<>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public List<Employee> getSubordinates() {
+		return subordinates;
+	}
+
+	public void add(Employee e) {
+	      subordinates.add(e);
+	   }
+
+	   public void remove(Employee e) {
+	      subordinates.remove(e);
+	   }
+
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", dept=" + dept + ", salary=" + salary + "]";
+	}
+	
+}
